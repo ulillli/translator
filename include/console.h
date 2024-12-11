a0e83b8f165d2c070b2f88afd1be25df9a93ef82
YuliyaPapulina <yuliyapap1510@gmail.com>
2024-12-11 22:47:39 +0300

    added some changes and cmake list

--- include/console.h
+++ include/console.h
@@ -43,33 +43,41 @@ public:
 	}
 	void Expression() {
 			std::string str;
-			syntaxAnalyzer S;
-			lexicalAnalyzer L;
+			std::cout << "You can keep your expression.\n";
+			std::cout << "1. If you entered an incorrect expression(you will be told about this later), \nyou need to correct the error and enter it again.\nThe program will query the expression until it gets the correct one.\n";
+			std::cout << "2. If you want to stop typing expressions, write stop.\n";
+			std::cout << "3. If the expression is correct and contains variables, then enter their values.\n";
+			std::cout << "4. Get the result.\n";
+			std::cout << "\nInput new expression\n";
 			std::cin >> str;
-			std::vector<Term*> v = L.getVectorOfTerms(str);
-			//printVectorOfTerm(v);
-			while (S.correctly(v) == 0) {
-				std::cout << "Expression is not correctly, input a new expression\n";
+			while (str != "stop") {
+				syntaxAnalyzer S;
+				lexicalAnalyzer L;
+				std::vector<Term*> v = L.getVectorOfTerms(str);
+				while (S.correctly(v) == 0) {
+					std::cout << "Expression is not correctly, input a new expression\n";
+					std::cin >> str;
+					v = L.getVectorOfTerms(str);
+					if (str == "stop") {
+						break;
+					}
+				}
+				std::cout << "You entered the correct expression\n";
+				translator T;
+				std::vector<Term*> t = T.getPostfix(v);
+				for (int i = 0; i < t.size(); i++) {
+					if (dynamic_cast<Variable*>(t[i])) {
+						double x;
+						std::cout << dynamic_cast<Variable*>(t[i])->getName() << "=";
+						std::cin >> x;
+						dynamic_cast<Variable*>(t[i])->setData(x);
+					}
+				}
+				calculator C;
+				double res = C.calculate(t);
+				std::cout << "Result: "<<res << std::endl;
+				std::cout << "Input new expression\n";
 				std::cin >> str;
-				v = L.getVectorOfTerms(str);
-				if (str == "stop") {
-					break;
-				}
 			}
-			//v = L.getVectorOfTerms(str);
-			translator T;
-			std::vector<Term*> t = T.getPostfix(v);
-			printVectorOfTerm(t);
-			for (int i = 0; i < t.size(); i++) {
-				if (dynamic_cast<Variable*>(t[i])) {
-					double x;
-					std::cout << dynamic_cast<Variable*>(t[i])->getName() << "=";
-					std::cin >> x;
-					dynamic_cast<Variable*>(t[i])->setData(x);
-				}
-			}
-			calculator C;
-			double res = C.calculate(t);
-			std::cout << res;
 	}
 };
--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,6 @@ int main() {
 	try {
 		workWithExpression W;
 		W.Expression();
-		std::cout << std::endl;
-		std::cout << (1 + 2) + 5 * (3 * (3 + 0)) * sinf(10) + (7);
-		//(a+b)+5*(3*(3+0))*sin(x)+(7))
 	}
 	catch (const char* str) {
 		std::cout << str;
